e35907d18a1b4a2a82eb164c2059b7d3c9f12e58
Mad-Chuck <michalmatczak22@gmail.com>
2023-09-11 19:16:02 +0200

    Added stage 5 and 7

--- src/data/read_cifar100.py
+++ src/data/read_cifar100.py
@@ -2,10 +2,12 @@ import os
 from torchvision import datasets, transforms
 
 
-def load_cifar100_data(data_dir: str) -> tuple:
+def load_cifar100_data(data_dir: str, transform=None) -> tuple:
     if not os.path.exists(os.path.join(data_dir, 'cifar-100-python')):
         raise FileNotFoundError(f"The CIFAR-100 dataset is not found in '{data_dir}'. Please download it first.")
-    transform = transforms.Compose([transforms.ToTensor()])
+
+    if transform is None:
+        transform = transforms.Compose([transforms.ToTensor()])
 
     train_dataset = datasets.CIFAR100(root=data_dir, train=True, download=False, transform=transform)
     test_dataset = datasets.CIFAR100(root=data_dir, train=False, download=False, transform=transform)
--- src/run.py
+++ src/run.py
@@ -1,7 +1,9 @@
 import time
 
 import torch
+from torch.nn.utils import prune
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
 from src.visualize.plot_dataset_images import plot_example_images
 from src.data.read_cifar100 import load_cifar100_data
@@ -13,18 +15,29 @@ from src.visualize.plot_train_metrics import plot_metrics
 
 # Consts
 PLOT_MODE = False
-EPOCHS = 5
-NOISE_LABELS = 0.1
+EPOCHS = 7
+NOISE_LABELS = 0.2
 DATA_DIR = 'data/raw'
 WATER_ANIMALS = sorted(['flatfish', 'lobster', 'dolphin', 'seal', 'shark'])
 LAND_ANIMALS = sorted(['lizard', 'bear', 'elephant', 'chimpanzee', 'squirrel'])
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+PRUNING_AMOUNT = 0.4
+FINE_TUNING_EPOCHS = 1
 print(f'Uses: {DEVICE} for torch.')
+
+transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(degrees=15),
+    transforms.RandomResizedCrop(size=32, scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+    transforms.ToTensor()
+])
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 # Prepare original dataset.
-org_train, org_test = load_cifar100_data(DATA_DIR)
+org_train, org_test = load_cifar100_data(DATA_DIR, transform=transform)
 land_animals_idx = [idx for idx, name in enumerate(org_train.classes) if name in LAND_ANIMALS]
 water_animals_idx = [idx for idx, name in enumerate(org_train.classes) if name in WATER_ANIMALS]
 
@@ -82,3 +95,39 @@ if PLOT_MODE:
     plot_metrics(train_losses, test_losses, test_accuracies)
 
 # ---------------------------------------------------------------------------------------------------------------------
+
+# Model sparsification for inference
+
+parameters_to_prune = []
+for name, module in model.named_modules():
+    if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
+        parameters_to_prune.append((module, 'weight'))
+
+prune.global_unstructured(
+    parameters_to_prune,
+    pruning_method=prune.L1Unstructured,
+    amount=PRUNING_AMOUNT,
+)
+
+print(f'After pruning')
+test(model, test_loader, criterion, DEVICE)
+
+sparse_optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+print(f'Fine tuning after pruning')
+
+train_losses = []
+test_losses = []
+test_accuracies = []
+for i in range(FINE_TUNING_EPOCHS):
+    print(f'[{i + 1}/{FINE_TUNING_EPOCHS}]')
+    train_loss = train(model, train_loader, criterion, sparse_optimizer, DEVICE)
+    train_losses.append(train_loss)
+
+    test_loss, test_acc = test(model, test_loader, criterion, DEVICE)
+    test_losses.append(test_loss)
+    test_accuracies.append(test_acc)
+    time.sleep(0.5)
+
+if PLOT_MODE:
+    plot_metrics(train_losses, test_losses, test_accuracies)
